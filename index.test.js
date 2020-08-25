09561df7f5a807bab2a8b212d8d85bb536de2302
tuskerette <valeria.graffeo@gmail.com>
2020-08-25 08:40:17 -0400

    add package.json

---
+++ index.test.js
@@ -0,0 +1,48 @@
+import { fireEvent, getByText } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+
+let dom
+let container
+
+describe('index.html', () => {
+  beforeEach(() => {
+  // Constructing a new JSDOM with this option is the key
+  // to getting the code in the script tag to execute.
+  // This is indeed dangerous and should only be done with trusted content.
+  // https://github.com/jsdom/jsdom#executing-scripts
+  dom = new JSDOM(html, { runScripts: 'dangerously' })
+  container = dom.window.document.body
+					    })
+
+  it('renders a heading element', () => {
+    expect(container.querySelector('h1')).not.toBeNull()
+    expect(getByText(container, 'Pick a TNG episode for me to watch tonight')).toBeInTheDocument()
+  })
+
+  it('renders a button element', () => {
+   expect(container.querySelector('button')).not.toBeNull()
+   expect(getByText(container, 'make it so')).toBeInTheDocument()
+  })
+
+ it('renders a new paragraph via JavaScript when the button is clicked', async () => {
+  const button = getByText(container, 'make it so')
+			        
+  fireEvent.click(button)
+  let seasonResults = container.querySelectorAll('#season')
+  expect(seasonResults.value).toBe(1)
+
+					        fireEvent.click(button)
+						    generatedParagraphs = container.querySelectorAll('#pun-container p')
+						        expect(generatedParagraphs.length).toBe(2)
+
+							    fireEvent.click(button)
+							        generatedParagraphs = container.querySelectorAll('#pun-container p')
+								    expect(generatedParagraphs.length).toBe(3)
+								      })
+})
+
--- readme.md
+++ readme.md
@@ -1,4 +1,18 @@
 There is this great free TV channel that shows Star Trek every night, but now I'm moving and I packed my TV.
+
 I can still watch Star Trek TNG online, but the beauty of watching it on TV is that they pick the episode for me, and I don't have to waste time in the rabbit hole.
+
 So I made [this](http://www.tuskerette.com/projects/tng-episode-picker)
+
 You're welcome :)
+
+### Install
+run `npm install`
+
+to run the jest test, run `jest <name-of-file.test.js>`
+
+### To do
+- [ ] add test with jest, just because
+- [ ] fetch episode title/info from some API (?)
+- [ ] link to watch on netflix/amazon prime video (?)
+
